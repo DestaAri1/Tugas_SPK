dbd7f347ff18a5898dc61988c67ba46d1d4ef6dc
DestaAri1 <desta.20008@mhs.unesa.ac.id>
2022-10-04 05:26:10 +0700

    main

---
+++ _header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Tugas SPK</title>
+    <link rel="stylesheet" href="_assets/css/bootstrap.min.css">
+    <link href="_assets/css/simple-sidebar.css" rel="stylesheet">
+    <script type="text/x-mathjax-config">MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}});</script>
+    <script type="text/javascript" async src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-MML-AM_CHTML"></script>
+</head>
+
+
+<body>
+    <script src="_assets/js/jquery-3.4.1.slim.min.js"></script>
+    <div id="wrapper">
+        <div id="sidebar-wrapper">
+            <ul class="sidebar-nav">
+                <li class="sidebar-brand">
+                    <a href="#"><span class="text-primary"><b>Desta Ari Alfanand</b></span></a>
+                </li>
+                <li>
+                    <a href="fuzzy.php">Perhitungan Fuzzy</a>
+                </li>
+            </ul>
+        </div>
+        <div id="page-content-wrapper">
+            <div class="container-fluid">
+                <a href="#menu-toggle" class="btn btn-outline-primary" id="menu-toggle">Menu</a>
